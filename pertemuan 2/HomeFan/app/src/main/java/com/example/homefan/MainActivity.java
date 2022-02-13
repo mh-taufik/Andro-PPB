8bd44a159ffd342e38e8de12f2419c0835bfb1f8
Muhammad Taufik <91010909+mh-taufik@users.noreply.github.com>
2022-02-13 12:00:47 +0700

    Update pertemuan 2

---
+++ pertemuan 2/HomeFan/app/src/main/java/com/example/homefan/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.homefan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.view.animation.LinearInterpolator;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+    ToggleButton toggelButton;
+    ImageView imageView;
+    ObjectAnimator rotateAnimator;
+    Switch switchButton;
+    SeekBar seekBar;
+    final int SPEED[] = {0, 5000, 3000, 4};
+    GradientDrawable gd = new GradientDrawable();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toggelButton = findViewById(R.id.toggleButton);
+        imageView = findViewById(R.id.imageView);
+        switchButton = findViewById(R.id.switch1);
+        seekBar = findViewById(R.id.seekBar3);
+
+        rotateAnimator=ObjectAnimator.ofFloat(imageView, "rotation", 0,360);
+        rotateAnimator.setDuration(1000);
+        rotateAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        rotateAnimator.setInterpolator(new LinearInterpolator());
+
+        gd.setShape(GradientDrawable.RECTANGLE);
+        gd.setGradientType(GradientDrawable.RADIAL_GRADIENT);
+        gd.setGradientRadius(330);
+
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    gd.setColors(new int[]{ Color.YELLOW , Color.TRANSPARENT });
+                    imageView.setBackground(gd);
+                }else{
+                    imageView.setBackgroundColor(Color.TRANSPARENT);
+                }
+            }
+        });
+        toggelButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    rotateAnimator.setDuration(SPEED[seekBar.getProgress()]);
+                    rotateAnimator.start();
+                }else{
+                    rotateAnimator.end();
+                }
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                rotateAnimator.setDuration(SPEED[seekBar.getProgress()]);
+                rotateAnimator.start();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
